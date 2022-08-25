0fb801c7e39859f2859b32cea314978fb13111d6
Ashan Devinda <37576256+ashanda@users.noreply.github.com>
2022-08-25 20:01:22 +0530

    cron job

---
+++ app/Console/Commands/notification.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+
+
+class notification extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'notification:cron';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'every 1 min update cron job in notification';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    { 
+        $expire_stamp = date('Y-m-d H:i:s', strtotime("+5 min"));
+        $now_stamp    = date("Y-m-d H:i:s");
+         $datas =  DB::table('users')
+                  ->join('notes','notes.emp_uid','users.id')
+                  ->where('notes.remind_me' ,'<', $now_stamp)  
+                  ->where('users.last_seen' ,'<', $expire_stamp)
+                  ->whereIn('users.type',  [3,2])
+                  ->get();
+        foreach( $datas as $data){
+           $matchThese = ['system_id'=>$data->note_id];
+     
+           Notification::updateOrCreate($matchThese,['system_id'=>$data->note_id, 'note'=>$data->note, 'reminder'=>$data->remind_me]);
+        }
+     
+        
+       
+    }
+}
--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,7 +4,7 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
-
+use Illuminate\Support\Facades\Log;
 class Kernel extends ConsoleKernel
 {
     /**
@@ -16,6 +16,14 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
+         $schedule->command('notification:cron')->timezone('Asia/Colombo')->everyMinute();
+       // $schedule->command('backup:run')->daily();
+
+        $schedule->call(function () {
+            Log::info("cron job is run");
+        })->timezone('Asia/Colombo')
+          ->dailyAt('02:00');
+        
     }
 
     /**
