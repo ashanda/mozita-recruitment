ea2b00c5464690e0c0af05b57f0ab62e60a8b54c
Ashan Devinda <37576256+ashanda@users.noreply.github.com>
2022-08-26 13:32:55 +0530

    modification 2022/08/26 1.21 p.m

--- app/Http/helpers.php
+++ app/Http/helpers.php
@@ -46,8 +46,8 @@ function notificatio_read_emp($id){
 
 function userDetails()
     {
-       $ip = '2402:4000:21c1:62d6:784d:1695:3aa4:1671'; //For static IP address get
-        //$ip = request()->ip(); //Dynamic IP address get
+       //$ip = '2402:4000:21c1:62d6:784d:1695:3aa4:1671'; //For static IP address get
+        $ip = request()->ip(); //Dynamic IP address get
         $data = Location::get($ip);
         
         return $data;
