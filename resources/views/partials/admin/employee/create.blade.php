47f7649b99b53444f89c3c672d4774c3d7d60774
Kasun24 <kasun961224@gmail.com>
2022-09-06 22:46:41 +0530

    done

--- resources/views/partials/admin/employee/create.blade.php
+++ resources/views/partials/admin/employee/create.blade.php
@@ -93,7 +93,7 @@
             <div class="col-xs-8 col-sm-8 col-md-8">
               <div id="dynamicAddRemove2">
                 <div class="row">
-                  <div class="col-xs-5 col-sm-5 col-md-5">
+                  <div class="col-xs-10 col-sm-10 col-md-10">
                     <div class="form-group">
                       <strong>Remarks :</strong>
                       <textarea name="addMoreInputFields[0][note]" class="form-control">
